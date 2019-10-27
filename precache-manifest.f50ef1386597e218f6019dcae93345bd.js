25a93f3e7dbd729870cb7cfaeaf954f1e009ae07
Mark <lmarkpaulo@gmail.com>
2019-10-27 16:53:33 +0800

    Deploy my portfolio website

---
+++ precache-manifest.f50ef1386597e218f6019dcae93345bd.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdad4e3ecce164952de6f2b30ae25c86",
+    "url": "/img/tools-icon/html5.jpg"
+  },
+  {
+    "revision": "f4cab4434f275a40dd4cdd6ee99f236f",
+    "url": "/img/works/ventusmine-full.jpg"
+  },
+  {
+    "revision": "afa2d413d75db1ba14d8",
+    "url": "/js/chunk-2d0e8bc5.14446c1f.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "310b0aba381a8b142358",
+    "url": "/js/chunk-ebdaf33a.965144fd.js"
+  },
+  {
+    "revision": "8d0a990ab9f001543eae",
+    "url": "/js/chunk-vendors.d3be6771.js"
+  },
+  {
+    "revision": "0fd249f2a17e6f2a2a5ed626d974b26e",
+    "url": "/img/works/ventusmine-intro.jpg"
+  },
+  {
+    "revision": "e01e528dfdedda1b7463",
+    "url": "/js/app.2aa1872b.js"
+  },
+  {
+    "revision": "8af2218a0056413d405907ed3122e57a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d351d13c70be3c4e253ef4f062b4cc38",
+    "url": "/img/works/alertforex-intro.jpg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "d94adac440772105d635b9439528b2fb",
+    "url": "/img/works/typochat-full.jpg"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "b4bcfe053c760050dba476d7c0e2f59d",
+    "url": "/img/works/solotrader-full.jpg"
+  },
+  {
+    "revision": "595f31b1d707e5a95fe7b76ed784214e",
+    "url": "/img/works/stdash-intro.jpg"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0202d6d870edfc03e184604df501845d",
+    "url": "/img/works/typochat-intro.jpg"
+  },
+  {
+    "revision": "c39dbbe924a549ed650d51b398decd9a",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "3d1071c8c7c610b8a06b0bc71c43ba45",
+    "url": "/img/brand/mlogo-dark.png"
+  },
+  {
+    "revision": "1d213a5b7060380c9eb5fca4bbbce565",
+    "url": "/img/brand/mlogo-light.png"
+  },
+  {
+    "revision": "d7096d2103c746c09dd42ede28a8f7be",
+    "url": "/img/tools-icon/bootstrap.jpg"
+  },
+  {
+    "revision": "7972f73bf40b79aaaad96911ecfef8bc",
+    "url": "/img/tools-icon/css3.jpg"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "5e0c8e842240182d6ed3ff77122dcb37",
+    "url": "/img/tools-icon/js.jpg"
+  },
+  {
+    "revision": "99b6c2241fcc26a00b4d125f821298e5",
+    "url": "/img/tools-icon/vue.jpg"
+  },
+  {
+    "revision": "08ed3767145ab5bf985654d3cfc35921",
+    "url": "/img/tools-icon/wordpress.jpg"
+  },
+  {
+    "revision": "eb48fdd776d427b5f32afb8150d8165d",
+    "url": "/img/thumb/2x2.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "8cd5c9b499603aa094428250cc34240e",
+    "url": "/img/works/br-intro.jpg"
+  },
+  {
+    "revision": "c474ff9e877e9c14d3599a01c9435b66",
+    "url": "/img/works/alertforex-full.jpg"
+  },
+  {
+    "revision": "f4da23fb1f4b800207b9a68c304e0493",
+    "url": "/img/works/fw1-intro.jpg"
+  },
+  {
+    "revision": "9cfe34e9ec26f0503f4d7c160af5a794",
+    "url": "/img/works/br-full.jpg"
+  },
+  {
+    "revision": "d0d4b19fae40efc639b7c8915b11cd7f",
+    "url": "/img/works/gmb-intro.jpg"
+  },
+  {
+    "revision": "f526d5161e5d6d11374eadadb67ec63e",
+    "url": "/img/works/gmb-full.jpg"
+  },
+  {
+    "revision": "4d66df8a8a9f1f0bbc19e2c657f03c96",
+    "url": "/img/works/payco-intro.jpg"
+  },
+  {
+    "revision": "65dffca051d8cca0738fa6f560cab1c6",
+    "url": "/img/works/fw1-full.jpg"
+  },
+  {
+    "revision": "319c9daffe47641b01dfe372f0805b1b",
+    "url": "/img/works/solotrader-intro.jpg"
+  },
+  {
+    "revision": "fbcc455e4f92d80f3c0ec920e909e9dd",
+    "url": "/img/works/stdash-full.jpg"
+  },
+  {
+    "revision": "9d3a51992b8ff16401ba83319ba077e7",
+    "url": "/img/works/payco-full.jpg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "c39dbbe924a549ed650d51b398decd9a",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "310b0aba381a8b142358",
+    "url": "/css/chunk-ebdaf33a.79c177b7.css"
+  },
+  {
+    "revision": "e01e528dfdedda1b7463",
+    "url": "/css/app.47ab081b.css"
+  }
+];
